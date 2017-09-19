59b5ed4e0048efca2a8e67c82e2f1d138a10176f
syahrul12345 <muhdsyahrulnizam123@gmail.com>
2017-09-19 15:24:03 +0800

    functionality to pull data from textField added

--- Desktop/AndroidProjects/EasyConvert/src/controller/agreeController.java
+++ Desktop/AndroidProjects/EasyConvert/src/controller/agreeController.java
@@ -1,10 +1,14 @@
 package controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
+
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -12,6 +16,11 @@ import java.util.ResourceBundle;
 public class agreeController implements Initializable{
 
     @FXML private GridPane gridPane;
+    @FXML private TextField value;
+    @FXML private TextField inUnits;
+    @FXML private TextField outUnits;
+    @FXML private TextArea textArea;
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -20,5 +29,12 @@ public class agreeController implements Initializable{
         col1.setHgrow(Priority.ALWAYS);
 
         gridPane.getColumnConstraints().add(col1);
+    }
+
+    public void convertBtnClicked(ActionEvent actionEvent) {
+        String fromValue = value.getText();
+        String fromIn = inUnits.getText();
+        String fromOut = outUnits.getText();
+        textArea.appendText(fromValue + fromIn + fromOut);
     }
 }
